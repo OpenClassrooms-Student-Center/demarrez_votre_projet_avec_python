26bc42afb24eef09bc872e6755fb4565c2c2d3c1
Céline MS <celine.martinet.sanchez+github@gmail.com>
2016-12-16 16:52:48 +0100

    program ready for review

--- san_antonio.py
+++ san_antonio.py
@@ -2,50 +2,81 @@ import subprocess
 import json
 import random
 
+# Parse a page using Scrapy
+# https://docs.python.org/3/library/subprocess.html
+def parse(scrapper_file, output_file):
+    subprocess.run(["rm", "-rf", output_file])
+    subprocess.run(["scrapy", "runspider", scrapper_file, "-o", output_file])
+
+# Give a Json file and return a Dictionary
+def open_json(file):
+    with open(file) as f:
+        data = json.load(f)
+        return data
+
+# Give a json and return a list
+def store_in_list(json_input, list_output, key):
+    # Store quotes on a list. Create an empty list and add each sentence one by one.
+    for sentence in json_input:
+        # Clean quotes from whitespace and so on
+        s = sentence[key].strip()
+        # don't use extend as it adds each letter one by one!
+        list_output.append(s)
+
+# Return a random item in a list
+def random_item_in(list):
+    rand_numb = random.randint(1, len(list))
+    return list[rand_numb]  
+
+
 #####################
 ###### QUOTES #######
 #####################
 
-# Gather quotes and put them into quotes.json
-# https://docs.python.org/3/library/subprocess.html
-def get_quotes():
-    subprocess.run(["rm", "-rf", "quotes.json"])
-    subprocess.run(["scrapy", "runspider", "san_antonio_scrapper.py", "-o", "s_a.json"])
-
-# Load quotes from a CSV file
-with open('s_a.json') as f:
-    data = json.load(f)
-
-# Store quotes on a list. Create an empty list and add each sentence one by one.
-quotes = []
-
-for sentence in data:
-    # Clean quotes from whitespace and so on
-    s = sentence['quote'].strip()
-    # don't use extend as it adds each letter one by one!
-    q = quotes.append(s)
-
-# Get any random quote
-rand_numb = random.randint(1, len(quotes))
-random quote = quotes[]
+def random_quote():
+    json_quotes = open_json('s_a.json')
+    quotes = []
+    store_in_list(json_quotes, quotes, 'quote')
+    return random_item_in(quotes) 
 
 ######################
-###### POLITICS ######
+#### CHARACTERS ######
 ######################
 
-# Gather politics
+# Gather characters from Wikipedia
 
-# Gather dates
+def random_character():
+    json_characters = open_json('characters.json')
+    characters = []
+    store_in_list(json_characters, characters, 'character')
+    return random_item_in(characters) 
 
-# Harmonise politics names
+######################
+###### SENTENCE ######
+######################
 
-# Create a dictionnary out of quotes and politics on this format: 
-# quotes = {'thatcher': {'quotes': ['Quote 1', 'Quote 2', 'Quote 3'], 'name': 'Thatcher'}}
-# quotes['thatcher']['quotes']
+######################
+#### INTERACTION ######
+######################
 
-# String creation, like "Thatcher said, in 1300: Quote"
+# Print a random sentence.
 
-# Randomly show one when we launch the program.
+def random_sentence():
+    rand_quote = random_quote()
+    rand_character = random_character()
+    print(">>>> {} a dit : {}".format(rand_character, rand_quote))
 
-# Propose to launch the program again to have another quote. 
-# We should ask him to refresh quotes and politics names.
+def interaction():
+    choice = input('Would you like another true quote? Type [enter] To exit, type [B]. To launch the scrapper again, type [C]').upper()
+    if choice == 'B':
+        quit()
+    elif choice == 'C':
+        parse('san_antonio_scrapper.py', 's_a.json')
+        parse('characters_scrapper.py', 'characters.json')
+    else:
+        random_sentence()
+        interaction()
+
+if __name__ == '__main__':
+    random_sentence()
+    interaction()
