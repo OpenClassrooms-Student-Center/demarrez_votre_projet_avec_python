63112555668e91afb1a86f248735301d12b81449
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-10-04 20:29:33 +0200

    P3C4

--- san_antonio.py
+++ san_antonio.py
@@ -1,4 +1,4 @@
-# -*- coding: latin-1 -*-
+# -*- coding: utf8 -*-
 
 quotes = [
     "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !",
@@ -15,25 +15,25 @@ characters = [
     "Kirikou"
 ]
 
-def show_random_quote():
+def message(character, quote):
+    n_character = character.capitalize()
+    n_quote = quote.capitalize()
+    return "{} a dit : {}".format(n_character, n_quote)
+
+def get_random_quote():
     # get a random number
     # get a quote from an array
     # show the quote in the interpreter
     pass
 
-def show_random_item_in(my_list):
+def get_random_item_in(my_list):
     # TODO: get a random number
     item = my_list[0] # get a quote from a list
     return item # return the item
 
-show_random_quote()
-print(show_random_item_in(quotes))
+# Programm
+user_answer = input('Tapez entrée pour connaître une autre citation ou B pour quitter le programme.')
 
-user_answer = input('Tapez entrée pour découvrir une autre citation ou B pour quitter le programme.')
-# if user_answer == "B":
-#     pass
-# elif user_answer == "C":
-#     print("C pas la bonne réponse ! Et G pas d’humour, je C...")
-# else:
-#     # show another quote
-#     pass
+while user_answer != "B":
+    print(message(get_random_item_in(characters), get_random_item_in(quotes)))
+    user_answer = input('Tapez entrée pour connaître une autre citation ou B pour quitter le programme.')
